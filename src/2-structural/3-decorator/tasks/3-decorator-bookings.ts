29c1dd1ef5000c6ac2ba0aa62f390734e8542c1b
Alberto Basalo <albertobasalo@hotmail.com>
2022-08-10 10:51:31 +0200

    fix: comment bad code

--- src/2-structural/3-decorator/tasks/3-decorator-bookings.ts
+++ src/2-structural/3-decorator/tasks/3-decorator-bookings.ts
@@ -22,17 +22,17 @@ export class Agency implements BookingCreator {
     const user = "";
     return new Booking(bookingId, trip, user, price, "Pending", new Date(), new Date());
   }
-  public cancelBooking(booking: Booking): Booking {
-    return new Booking(
-      booking.id,
-      booking.trip,
-      booking.user,
-      booking.price,
-      "Cancelled",
-      booking.createdAt,
-      new Date()
-    );
-  }
+  // public cancelBooking(booking: Booking): Booking {
+  //   return new Booking(
+  //     booking.id,
+  //     booking.trip,
+  //     booking.user,
+  //     booking.price,
+  //     "Cancelled",
+  //     booking.createdAt,
+  //     new Date()
+  //   );
+  // }
 }
 
 export class AgencyCanceller implements BookingCreator {
